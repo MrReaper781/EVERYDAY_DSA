00d0440a366f562b5d3d33430633c82ca0a048f6
Vaibhav Vishesh <140585921+MrReaper781@users.noreply.github.com>
2025-03-11 20:44:08 +0530

    twenteeth commit

--- IntegerHashing.java
+++ IntegerHashing.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class IntegerHashing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int arr[] = new int[a];
-        HashMap<Integer, Integer> mpp = new HashMap<>();
+        // int a = sc.nextInt();
+        // int arr[] = new int[a];
+        // HashMap<Integer, Integer> mpp = new HashMap<>();
+        sc.close();
     }
 }
